cefe5de334527df0cc4bcc126c670e6019b2744e
daranzolin <daranzolin@gmail.com>
2018-02-22 20:39:34 -0800

    included pipe

--- R/viewPipeChain.R
+++ R/viewPipeChain.R
@@ -1,6 +1,6 @@
 viewPipeChain <- function() {
   pc <- rstudioapi::getActiveDocumentContext()
-  if (!grepl("%>%", pc)) stop("Must highlight a pipe sequence")
+  #if (!grepl("%>%", pc)) stop("Must highlight a pipe sequence")
   pc <- pc$contents
   pcTitles <- createViewTitles(pc)
   pcCalls <- createCalls(pc)
