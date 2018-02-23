6aad96d064c53cdca6ac20713ca50edb68bbfcd0
daranzolin <daranzolin@gmail.com>
2018-02-22 20:33:09 -0800

    initial commit

---
+++ R/utils.R
@@ -0,0 +1,28 @@
+createCalls <- function(x) {
+  x <- map_chr(x, stringr::str_trim)
+  calls <- list()
+  for (i in 2:(length(x))) {
+    if (grepl("group_by", x[i])) next
+    if (i != length(x)) {
+      call <- paste(paste(x[1:i], collapse = " "), "View(")
+    } else {
+      call <- paste(paste(x[1:i], collapse = " "), "%>% View(")
+    }
+    calls[[i]] <- call
+  }
+  invisible(purrr::discard(calls, is.null))
+}
+
+createViewTitles <- function(steps) {
+  gbs <- sum(stringr::str_detect(steps, "group_by"))
+  stepInds <- 2:(length(steps) - gbs)
+  steps <- tail(purrr::map(strsplit(x, "\\("), `[[`, 1), -1) %>%
+    purrr::discard(~grepl("group_by", .)) %>%
+    purrr::map_chr(str_trim)
+  sprintf("%s. %s", (stepInds - 1), steps)
+}
+
+createViews <- function(calls, titles) {
+  calls <- sprintf("%s title = '%s')", calls, titles)
+  invisible(purrr::map(calls, ~eval(parse(text = .))))
+}
---
+++ R/viewPipeChain.R
@@ -0,0 +1,9 @@
+viewPipeChain <- function() {
+  pc <- rstudioapi::getActiveDocumentContext()
+  if (!grepl("%>%", pc)) stop("Must highlight a pipe sequence")
+  pc <- pc$contents
+  pcTitles <- createViewTitles(pc)
+  pcCalls <- createCalls(pc)
+  createViews(pcCalls, pcTitles)
+}
+
